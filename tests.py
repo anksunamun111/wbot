ba63689929f6748ced1998794b8281bc56e5ef03
haxer <abisalov10@mail.ru>
2022-02-15 14:26:31 +0300

    updated tests

--- tests.py
+++ tests.py
@@ -2,10 +2,6 @@ import unittest
 
 class TgBotTests(unittest.TestCase):
 
-    def test_d(self):
-        from wbot import f
-        self.assertEqual(f(), 2)
-
     def test_get_location(self):
         from wbot import get_location
         self.assertIsNotNone(get_location('88', '77'))
@@ -13,6 +9,11 @@ class TgBotTests(unittest.TestCase):
     def test_owm_token_is_exists(self):
         import config
         self.assertIsNotNone(config.TOKEN)
+
+    def test_get_weather(self):
+        with self.assertRaises(Exception):
+           from wbot import get_weather
+           get_weather("вв")
 
 if __name__ == "__main__":
     unittest.main()
